dd956172734f5559ddafbd677304a2efbae8533f
Tony Green <tgreen@albatrossflavour.com>
2021-06-15 19:22:21 +1000

    initial

---
+++ main.py
@@ -0,0 +1,30 @@
+import random
+from flask import Flask
+
+app = Flask(__name__)
+
+name = [
+        "Ringo",
+        "Danny Boy",
+        "Ringmaster",
+        "Dan the man",
+        "New guy",
+        "Danno",
+        "He who books people",
+        "Book him Danno",
+        "Not Paul, George or John",
+        "That guy from Transurban",
+        "Dan with the van",
+        "d4n0",
+        "01100100 01100001 01101110 00001101 00001010",
+        "The ring bearer",
+        "Ring-a-ding-ding",
+        "Frodo",
+        "DR",
+        "DRing",
+        "Dan"
+        ]
+
+@app.route("/")
+def index():
+    return random.choice(name)
